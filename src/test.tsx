0dec1b37f1bdb68d785df0525da7302e159d9c6b
manteca-roja <luisbrandonbarrones@gmail.com>
2023-11-27 16:17:59 +0100

    script in component

--- src/test.tsx
+++ src/test.tsx
@@ -1,27 +1,45 @@
-import { useEffect } from "react";
-import React from "react";
-export default function ScriptWithComponent() {
+import React, { useEffect } from "react";
+
+const BySideScript = () => {
+  const addScriptToDocument = () => {
+    setTimeout(() => {
+      // Inline script with variables
+      const inlineScript = document.createElement("script");
+      inlineScript.text = `var bysideWebcare_webcare_id="7B0E5D0519",bysideWebcare_lang="pt",bysideWebcare_channel="fiat"`;
+      inlineScript.async = true;
+      inlineScript.nonce = "MTo3NzMwMToyOTM1MDE0NTU1OjE3MDEwNzI4ODU="; // Adding nonce value
+
+      // Append the inline script to the head
+      document.head.appendChild(inlineScript);
+
+      // Function to execute when the external script is loaded
+      const handleScriptLoad = () => {
+        console.log("script loaded!");
+        // Your code that depends on bysideWebcare_webcare_id goes here
+        // bysideWebcare_createQueryStringParams();
+      };
+
+      // Load external script after the inline script
+      const externalScript = document.createElement("script");
+      externalScript.type = "text/javascript";
+      externalScript.src = "https://bywe2.byside.com/agent/bwc_we2.js";
+
+      externalScript.async = true;
+      externalScript.nonce = "MTo3NzMwMToyOTM1MDE0NTU1OjE3MDEwNzI4ODU="; // Adding nonce value
+
+      // Attach the onload event for modern browsers
+      externalScript.onload = handleScriptLoad;
+
+      // Append the external script to the head
+      document.head.appendChild(externalScript);
+    }, 5000);
+  };
+
   useEffect(() => {
-    const firstScript = document.createElement("script");
-    firstScript.text = `
-          var
-          bysideWebcare_webcare_id="7B0E5D0519",bysideWebcare_lang="pt",bysideWebcare_channel="kiavibe"
-          
-        `;
-
-    document.head.appendChild(firstScript);
-
-    const secondScript = document.createElement("script");
-    secondScript.src = "https://bywe2.byside.com/agent/bwc_we2.js";
-    secondScript.async = true;
-
-    document.head.appendChild(secondScript);
-
-    return () => {
-      document.head.removeChild(firstScript);
-      document.head.removeChild(secondScript);
-    };
+    addScriptToDocument();
   }, []);
 
   return <></>;
-}
+};
+
+export default BySideScript;
