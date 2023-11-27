294fe926b7b2687e3d3b2df0e9092187b70d5c99
manteca-roja <luisbrandonbarrones@gmail.com>
2023-11-27 16:35:12 +0100

    xd

--- src/test.tsx
+++ src/test.tsx
@@ -2,37 +2,35 @@ import React, { useEffect } from "react";
 
 const BySideScript = () => {
   const addScriptToDocument = () => {
-    setTimeout(() => {
-      // Inline script with variables
-      const inlineScript = document.createElement("script");
-      inlineScript.text = `var bysideWebcare_webcare_id="7B0E5D0519",bysideWebcare_lang="pt",bysideWebcare_channel="fiat"`;
-      inlineScript.async = true;
-      inlineScript.nonce = "MTo3NzMwMToyOTM1MDE0NTU1OjE3MDEwNzI4ODU="; // Adding nonce value
+    // Inline script with variables
+    const inlineScript = document.createElement("script");
+    inlineScript.text = `var bysideWebcare_webcare_id="7B0E5D0519",bysideWebcare_lang="pt",bysideWebcare_channel="kia"`;
+    inlineScript.async = true;
+    inlineScript.nonce = "MTo3NzMwMToyOTM1MDE0NTU1OjE3MDEwNzI4ODU="; // Adding nonce value
 
-      // Append the inline script to the head
-      document.head.appendChild(inlineScript);
+    // Append the inline script to the head
+    document.head.appendChild(inlineScript);
 
-      // Function to execute when the external script is loaded
-      const handleScriptLoad = () => {
-        console.log("script loaded!");
-        // Your code that depends on bysideWebcare_webcare_id goes here
-        // bysideWebcare_createQueryStringParams();
-      };
+    // Function to execute when the external script is loaded
+    const handleScriptLoad = () => {
+      console.log("script loaded!");
+      // Your code that depends on bysideWebcare_webcare_id goes here
+      // bysideWebcare_createQueryStringParams();
+    };
 
-      // Load external script after the inline script
-      const externalScript = document.createElement("script");
-      externalScript.type = "text/javascript";
-      externalScript.src = "/byside.js";
+    // Load external script after the inline script
+    const externalScript = document.createElement("script");
+    externalScript.type = "text/javascript";
+    externalScript.src = "/byside.js";
 
-      externalScript.async = true;
-      externalScript.nonce = "MTo3NzMwMToyOTM1MDE0NTU1OjE3MDEwNzI4ODU="; // Adding nonce value
+    externalScript.async = true;
+    externalScript.nonce = "MTo3NzMwMToyOTM1MDE0NTU1OjE3MDEwNzI4ODU="; // Adding nonce value
 
-      // Attach the onload event for modern browsers
-      externalScript.onload = handleScriptLoad;
+    // Attach the onload event for modern browsers
+    externalScript.onload = handleScriptLoad;
 
-      // Append the external script to the head
-      document.head.appendChild(externalScript);
-    }, 5000);
+    // Append the external script to the head
+    document.head.appendChild(externalScript);
   };
 
   useEffect(() => {
