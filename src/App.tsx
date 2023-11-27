e58e5e340f800d5ae92fdfe08d5b23d1dc186c29
manteca-roja <luisbrandonbarrones@gmail.com>
2023-11-27 16:03:19 +0100

    added script for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <script>
+          {`
+          var
+          bysideWebcare_webcare_id="7B0E5D0519",bysideWebcare_lang="pt",bysideWebcare_channel="kiavibe"
+          `}
+        </script>
+        <script
+          src="https://bywe2.byside.com/agent/bwc_we2.js"
+          type="text/javascript"
+        ></script>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
