b6b1ead4a75da7c19b4b8cc58fb9f0e04577e322
manteca-roja <luisbrandonbarrones@gmail.com>
2023-11-29 11:28:36 +0100

    added remote script

--- src/script.tsx
+++ src/script.tsx
@@ -5,10 +5,14 @@ const AnotherBySideScript = () => {
   return (
     <>
       <Helmet>
+        <script
+          type="text/javascript"
+          src="https://bywe2.byside.com/agent/bwc_we2.js"
+          async
+        ></script>
         <script>
           {`var bysideWebcare_webcare_id="7B0E5D0519",bysideWebcare_lang="pt",bysideWebcare_channel="kiavibe"`}
         </script>
-        <script type="text/javascript" src="/byside.js" async></script>
       </Helmet>
     </>
   );
